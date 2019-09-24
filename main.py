deb0cb7eed5f6b8601ee0000003a3895ade1418b
Sakuk3 <lukas.fritsch30@gmail.com>
2019-09-24 20:51:12 +0200

    Added Multiprocessing

--- main.py
+++ main.py
@@ -1,23 +1,70 @@
 from PIL import Image
 import numpy as np
+from multiprocessing import Process, Queue
+import time
+
 def main():
-    size_x = 100
-    size_y = 100
+    width = 1920
+    hight = 1080
     start_x = -2.5
     stop_x = 1
     start_y = -1.2
     stop_y = 1.2
-    max_iter = 100
+    max_iter = 5
+    thread_count = 4
+    output_path = "renders/{}.jpg"
 
-    mandelbrot_array = mandelbrot(size_y,size_x,start_x,stop_x,start_y,stop_y,max_iter)
-    mandelbrot_image = Image.fromarray(np.uint8(mandelbrot_array))
-    mandelbrot_image.save("1.jpg")
-    
-def mandelbrot(size_y,size_x,start_x,stop_x,start_y,stop_y,max_iter):
-    result = np.zeros([size_y,size_x,3])
-    for iy,ix in np.ndindex((size_y,size_x)):
-        scaled_x = scale(ix,(0,size_x),(start_x, stop_x))
-        scaled_y = scale(iy,(0,size_y),(start_y, stop_y))
+    for i in range(5,100,5):
+        print("sterted Process with max_iter: {}".format(i))
+        output_array = mandelbrot_multiprocessing(hight,width,start_x,stop_x,start_y,stop_y,i,thread_count)
+
+        output_image = Image.fromarray(np.uint8(output_array))
+        output_image.save(output_path.format(i))
+
+def mandelbrot_multiprocessing(hight,width,start_x,stop_x,start_y,stop_y,max_iter,thread_count):
+    jobs = []
+    result_queue = Queue()
+    output_array = np.zeros([hight,width,3])
+
+    with_slice_size = int(width/thread_count)
+    x_slice_size = (stop_x-start_x)/thread_count
+    for thread_number in range(thread_count):
+        proc = Process(target=mandelbrot_process, args=(
+            result_queue,
+            with_slice_size*thread_number,
+            hight,
+            with_slice_size,
+            start_x+x_slice_size*thread_number,
+            start_x+x_slice_size*thread_number + x_slice_size,
+            start_y,
+            stop_y,
+            max_iter))
+        jobs.append(proc)
+        proc.start()
+
+    iter = 0
+    while iter != thread_count:
+        if not result_queue.empty():
+            res = result_queue.get()
+            for iy,ix in np.ndindex(res["result"].shape[:2]):
+                output_array[iy][ix+res["x_offset"]] = res["result"][iy][ix]
+            iter +=1
+        else:
+            time.sleep(0.1)
+
+    return output_array
+
+def mandelbrot_process(result_queue,x_offset,hight,width,start_x,stop_x,start_y,stop_y,max_iter):
+    result_queue.put({
+        "x_offset":x_offset,
+        "result":mandelbrot(hight,width,start_x,stop_x,start_y,stop_y,max_iter)
+    })
+
+def mandelbrot(hight,width,start_x,stop_x,start_y,stop_y,max_iter):
+    result = np.zeros([hight,width,3])
+    for iy,ix in np.ndindex((hight,width)):
+        scaled_x = scale(ix,(0,width),(start_x, stop_x))
+        scaled_y = scale(iy,(0,hight),(start_y, stop_y))
         x = 0.0
         y = 0.0
         iteration = 0
@@ -26,13 +73,16 @@ def mandelbrot(size_y,size_x,start_x,stop_x,start_y,stop_y,max_iter):
             y = 2*x*y + scaled_y
             x = xtemp
             iteration = iteration + 1
-        result[iy][ix] = color_gradiant(scale(iteration,(0,max_iter),(0,1)))
+        result[iy][ix] = color_gradiant(scale(iteration,(0,max_iter),(0,1)),max_iter)
     return result
 
-def color_gradiant(iterations):
-    return (scale(iterations,(0,1),(0,255)),
-            scale(iterations,(0,1),(0,255)),
-            scale(iterations,(0,1),(0,255)))
+def color_gradiant(iterations,max_iter):
+    color_shema = [(0,0,0),(255,255,255)]
+    mix = iterations
+    c1 = np.array(color_shema[0])
+    c2 = np.array(color_shema[1])
+    res_color = mix*c1 + (1-mix)*c2
+    return res_color
 
 def scale(val, src, dst):
     """
